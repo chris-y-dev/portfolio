fa14853a84a42741108fb827563f3ae86abafd43
chrisyuen976 <chrisyuen976@gmail.com>
2023-02-18 18:33:03 +1100

    Styling footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.scss";
 import SocialData from "../../interfaces/SocialData";
+import fetchStaticImage from "../../hooks/fetchStaticImage";
 
 const Footer = () => {
   const socialData: SocialData[] = [
@@ -10,8 +11,8 @@ const Footer = () => {
   ];
 
   return (
-    <div className="border-blue row w-100 m-0" id="socials">
-      <div className="py-5 ps-5 pe-3 socials_container footer-containers_sizing col-md-5 col-sm-12">
+    <div className="border-blue row m-0" id="socials">
+      <div className="py-5 ps-5 pe-5 socials_container footer-containers_sizing col-md-5 col-sm-12">
         <h2>AVAILABLE FOR SELECT FREELANCE OPPORTUNITIES</h2>
         <ul className="mt-5">
           {socialData.map((item) => {
@@ -23,8 +24,18 @@ const Footer = () => {
           })}
         </ul>
       </div>
-      <div className="p-5 second_container footer-containers_sizing col-md-7 col-sm-12">
-        Div 2
+      <div className="p-5 second_container footer-containers_sizing col-md-7 col-sm-12 d-flex flex-column justify-content-between">
+        <div className="profile_container">
+          <div className="image_frame">
+            <img src={fetchStaticImage("/footer/profile-pic.jpg")} />
+          </div>
+        </div>
+        <img
+          src={fetchStaticImage("/footer/connect-with-me_crop.gif")}
+          alt="Connect with me"
+          className="typography"
+        />
+        <p className="text-end">VERSION 1.0</p>
       </div>
     </div>
   );
