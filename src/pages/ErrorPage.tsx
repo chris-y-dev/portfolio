cf22776df04c55fd2d6da7cae48a5e676ac9170a
chrisyuen976 <chrisyuen976@gmail.com>
2023-03-06 16:14:53 +1100

    Error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import {useRouteError} from "react-router-dom";
-
+import { useRouteError } from "react-router-dom";
+import "../App.scss";
 
 const ErrorPage = () => {
-
-    const error = useRouteError();
-    console.error(error);
-
+  const error = useRouteError();
+  console.error(error);
 
   return (
-  <div>
-    <h1>Error</h1>
-    {/* <p>{error.statusText || error.message}</p> */}
-  </div>)
+    <div>
+      <h1>Error</h1>
+      <p>
+        Page Not Found. Please go back to the{" "}
+        <a href="https://chris-y.pages.dev" className="error-link">
+          Homg Page
+        </a>{" "}
+        and try again!
+      </p>
+    </div>
+  );
 };
 
 export default ErrorPage;
