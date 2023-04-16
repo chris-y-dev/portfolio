e1f74e7e7aaccc4104a5e58e29f55f25942bc8df
chrisyuen976 <chrisyuen976@gmail.com>
2023-04-16 23:03:23 +1000

    Data typo fix

--- src/assets/data/ExperienceAccordionData.ts
+++ src/assets/data/ExperienceAccordionData.ts
@@ -7,7 +7,7 @@ const ExperienceAccordionData: ExperienceAccordion[] = [
     workType: "Full-time",
     workPeriod: "November 2022 - February 2023",
     description:
-      "Worked in a Backend team that specialised in API development at an ASX-listed accounting software company. I was responsible for developing features, debugging, writing unit tests, and configuring Docker containers. Opportunity to use enterprise-level tools include Jira, TeamCity pipelines and Confluence.\n\nMy greatest achievement was creating an new learning guide for future interns joining this team, since this team had not previously had an intern.",
+      "Worked in a back-end team that specialised in API development at an ASX-listed accounting software company. I was responsible for developing features, debugging, writing unit tests, and configuring Docker containers. Experienced elements of Extreme Programming and contributed in an Agile environment.\n\nMy greatest achievement was creating a new learning projects for future interns joining this team, since this team had not previously had an intern.",
     companyImagePath: "/experience/xero_logo.png",
     technologies: ["C#", "Dotnet", "Docker", "AWS", "TeamCity", "Git"],
   },
@@ -17,7 +17,7 @@ const ExperienceAccordionData: ExperienceAccordion[] = [
     workPeriod: "July 2022 - Ongoing",
     workType: "Part-time",
     description:
-      "Developing custom web-applications and CRMs for clients from various industries. My role requires me to be autonomous and pro-active, taking on various responsbilities in a small software company. Also responsible for researching and learning new APIs/libraries, and creating documentation for internal use.\n\nMy biggest achievement was leading the development of a new e-commerce app that incorporates 3D models.",
+      "Developing custom web-applications and CRMs at a software company, for clients from various industries. My role requires me to be autonomous, proactively make decisions, take initiatives to learn new technologies/libraries, and communicate with various stakeholders through creating clear documentation.\n\nMy biggest achievement was leading the development of a new e-commerce app that incorporates 3D models.",
     companyImagePath: "/experience/pegboardco_logo.png",
     technologies: [
       "TypeScript",
@@ -34,7 +34,7 @@ const ExperienceAccordionData: ExperienceAccordion[] = [
     workType: "Casual",
     workPeriod: "May 2022 - Feb 2023",
     description:
-      "Responsible for maintenance, debugging and development of multiple legacy websites for a piano distribution business. Work invovled using code and WordPress WooCommerce.\n\nMy greatest achievement was producing the initial design and hi-fidelity prototype of the new website, balancing between complex UX and business requirements.",
+      "Responsible for the maintenance, debugging and development of multiple legacy websites for a piano distribution business. Work invovled using code and WordPress WooCommerce.\n\nMy greatest achievement was producing the initial design and hi-fidelity prototype of the new website, balancing between complex UX and business requirements.",
     companyImagePath: "/experience/bml_logo.jpg",
     technologies: [
       "HTML",
