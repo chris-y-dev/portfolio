61a92bd32a31fb10b6774cb528ce2b1f5256ae13
chrisyuen976 <chrisyuen976@gmail.com>
2024-02-05 16:45:42 +1100

    expertise data

--- src/assets/data/ExpertiseData.ts
+++ src/assets/data/ExpertiseData.ts
@@ -2,16 +2,16 @@ import IExpertiseData from "../../interfaces/IExpertiseData";
 
 const ExpertiseData: IExpertiseData[] = [
   {
-    expertiseTitle: "Software Dev",
+    expertiseTitle: "Software Engineering",
     description:
-      "Commercial experience developing WebAPIs and Angular applications with Monolith and Microservices architectures. Familiar with SOLID principles and CLEAN architecture, particularly Depency Injection.",
+      "Enterprise-level experience implementing API and web applications with Monolith or Microservices architectures. Familiar with SOLID principles and CLEAN architecture, particularly Single Responsible Priciple & Depency Injection.",
     descriptionHighlight: undefined,
     graphicPath: "/expertise/software-development.png",
   },
   {
     expertiseTitle: "Frontend Dev",
     description:
-      "1.5+ years of commercial front-end experience in HTML, CSS, JS, React, and Angular frameworks.",
+      "1.5+ years of enterprise-level front-end experience using old & new frameworks (Vue2, Vue3, Angular). Preference for Typescript for code readibility.",
     descriptionHighlight:
       "Meticulous about Responsiveness - Try on Mobile/Resize this site!",
     graphicPath: "/expertise/frontend-development.png",
@@ -19,7 +19,7 @@ const ExpertiseData: IExpertiseData[] = [
   {
     expertiseTitle: "Web Design",
     description:
-      "Passionate about designing modern websites for small businesses, informed by UX/UI principles.",
+      "Through a side hobby, I have experience designing simple and modern websites for small businesses, informed by UX/UI principles.",
     descriptionHighlight: undefined,
     graphicPath: "/expertise/web-design.png",
   },
